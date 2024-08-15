95a9fa0a5ec25050244dccccc59f0e2722b14e22
gracebir <grace.devolop@gmail.com>
2024-08-15 09:40:41 +0200

    message

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import express from 'express'
 import cors from 'cors'
 import authRouter from './routes/auth.route.js'
 import connectDb from './db/connectDb.js'
+import messageRoute from './routes/message.route.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors())
 
 app.use("/api/auth", authRouter)
+app.use("/api/message", messageRoute)
 
 const port = process.env.PORT || 5000;
 
