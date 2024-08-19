d15a34d65fb4b57cc6fcd56e0a5158d82b0f8aa7
gracebir <grace.devolop@gmail.com>
2024-08-19 22:25:58 +0200

    sameSite

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -5,6 +5,7 @@ const generateTokenAndSetCookie =  (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 15 * 24 * 60 * 60 * 1000,
         httpOnly: true,
+        sameSite: "None",
         secure: process.env.NODE_ENV !== "development"
     })
 }
